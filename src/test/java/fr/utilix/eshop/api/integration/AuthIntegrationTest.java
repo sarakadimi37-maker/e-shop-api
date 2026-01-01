e51435a0afad59e5f7c5d81dba632269e9603944
sarakadimi37 <sarakadimi37@gmail.com>
2026-01-01 22:48:47 +0100

    maj application-integration.yml integration-tests

--- src/test/java/fr/utilix/eshop/api/integration/AuthIntegrationTest.java
+++ src/test/java/fr/utilix/eshop/api/integration/AuthIntegrationTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @ActiveProfiles("integration")
-@AutoConfigureMockMvc
+@AutoConfigureMockMvc(addFilters = false)
 class AuthIntegrationTest {
 
     @Autowired
