0e09fb1b27a7862dc10fde3a4b1812b8b83e85d6
sarakadimi37 <sarakadimi37@gmail.com>
2026-01-01 23:05:53 +0100

    delete (addFilters = false)

--- src/test/java/fr/utilix/eshop/api/integration/AuthIntegrationTest.java
+++ src/test/java/fr/utilix/eshop/api/integration/AuthIntegrationTest.java
@@ -21,7 +21,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @ActiveProfiles("integration")
-@AutoConfigureMockMvc(addFilters = false)
+@AutoConfigureMockMvc
 class AuthIntegrationTest {
 
     @Autowired
